0e2d324ecbbb08380d988078ce62810235b81bfe
mubarak <abubakrobasanjo@gmail.com>
2025-10-22 06:08:18 -0700

    hooks needed

---
+++ src/hooks/useClearance.js
@@ -0,0 +1,4 @@
+import { useClearance as useClearanceContext } from '../context/ClearanceContext';
+
+// Re-export the useClearance hook from context for convenience
+export const useClearance = useClearanceContext;
